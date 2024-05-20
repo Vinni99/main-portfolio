300d96446348fdbb6dc5f35971f686f9f2518b95
Vinicius <Zizoocz@gmail.com>
2024-05-20 00:30:18 -0700

    added style at the nav-links and icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
+import '../styles/navigation.css';
+import { SiCodingninjas } from "react-icons/si";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <>
-      <Nav className="justify-content-end" variant='pills' defaultActiveKey='#Portfolio'>
+      <Nav id="nav-link" className="" variant='pills' defaultActiveKey='#AboutMe'>
+        <Nav.Item className="ninja-icon" >
+          <SiCodingninjas />
+        </Nav.Item>
         <Nav.Item>
           <Nav.Link href="#portfolio"
-           onClick={() => handlePageChange("Portfolio")}
+            onClick={() => handlePageChange("Portfolio")}
             className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="#about-me" 
-          onClick={() => handlePageChange("About")}
+          <Nav.Link href="#about-me"
+            onClick={() => handlePageChange("About")}
             className={currentPage === "About" ? "nav-link active" : "nav-link"}>About Me</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="#contact" 
-          onClick={() => handlePageChange("Contact")}
+          <Nav.Link href="#contact"
+            onClick={() => handlePageChange("Contact")}
             className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>Contact</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="#resume" 
-          onClick={() => handlePageChange("Resume")}
+          <Nav.Link href="#resume"
+            onClick={() => handlePageChange("Resume")}
             className={currentPage === "Resume" ? "nav-link active" : "nav-link"}>Resume</Nav.Link>
         </Nav.Item>
-      </Nav> 
+      </Nav>
     </>
   );
 }
