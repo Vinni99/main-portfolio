3f990837a7d04b7614aee8e3a5e109fd1d9aa8f2
Vinicius <Zizoocz@gmail.com>
2024-05-20 23:41:19 -0700

    added picture inside the about page and style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import MainHomepage from './components/MainCard';
 
+
 function App() {
   return (
     <div className="HomePage">
       <MainHomepage/>
+      
     </div>
   );
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Portfolio() {
     return (
-        <div>
-            {/* Your portfolio content goes here */}
-        </div>
-    );
+        <Container id="portfolio" className="my-5">
+            <Row className="portfolio">
+                <Col md={6}>
+                    <h1>Portfolio</h1>
+                    <hr></hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis neque quis ipsum laoreet commodo.
+                        Phasellus commodo, purus quis consectetur rh);
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 export default Portfolio;
