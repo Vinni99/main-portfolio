303d18edf1947871e5f3a0956c8fd71b863f747d
Vinicius <Zizoocz@gmail.com>
2024-05-21 21:02:09 -0700

    added some updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
-import React from 'react';
-import MainHomepage from './components/MainCard';
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
 
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  const renderPage = () => {
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+  };
+
   return (
     <div className="HomePage">
-      <MainHomepage/>
-      
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <App />
+      <Footer />
+
     </div>
   );
 }
