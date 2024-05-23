4778c943d541834428ea679a4b36e6eb92f2a2bd
Vinicius <Zizoocz@gmail.com>
2024-05-22 23:00:04 -0700

    added the footer, icons & style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "../styles/footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
-        <footer>
-            <h2>Footer</h2>
+        <footer className="footerEl">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <ul className="list-inline">
+                            <li className="list-inline-item">
+                                <a href="https://www.linkedin.com/in/vinicius-teixeira-468a55269/" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon className="linkedin" icon={faLinkedin} size="2x" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="https://github.com/Vinni99" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon className="github" icon={faGithub} size="2x" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            <p className="create">© 2024 | Created by Vinni</p>
+            </div>
         </footer>
     );
 }
