f02be41061a9c3e1c8297c667c226ed01219becd
Vinicius <Zizoocz@gmail.com>
2024-06-08 16:32:43 -0700

    in resume.js I fixed the resume link, now allowing open the link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
         <div className="resume">
             <h1> Resume</h1>
             <hr />
-            <p>Download my <a href="https://drive.google.com/file/d/1CfOmwdI3aNdYa9Q3xYpJLt33vDeMOhOm/view">Resume</a></p>
+            <p>Download my <a href="https://drive.google.com/file/d/1CfOmwdI3aNdYa9Q3xYpJLt33vDeMOhOm/view?usep=sharing" target="_blank" rel="noopener noreferrer">Resume</a></p>
 
             <h2>Front-end Proficiencies</h2>
             <ul>
