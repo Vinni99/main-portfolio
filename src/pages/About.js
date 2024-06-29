dac160f19bbd1663b9dda6de8dafd9811ca9ed93
Vinicius <Zizoocz@gmail.com>
2024-06-29 10:47:04 -0700

    added some changes and style

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import AboutMe from '../img/profile.pic.png';
-import '../styles/about.css';
+import AboutMe from "../img/profile.pic.png";
+import "../styles/about.css";
 
 function About({ handleButtonClick }) {
-
   return (
     <Container className="my-5">
       <Row className="about-me">
@@ -12,19 +11,23 @@ function About({ handleButtonClick }) {
           <h1>About Me</h1>
           <hr></hr>
           <p>
-            Hey there, I'm Vinni. Enthusiastic Full Stack Developer ready to embark on my first professional adventure in the tech industry.
-            My journey so far has been filled with coding, learning, and building projects that I'm proud of.
-            Now, I'm looking for a chance to bring my skills to a forward-thinking, collaborate, grow and create innovative solutions.
-            Let's start something great!
+            Hey there, I'm Vinni. Enthusiastic Full Stack Developer ready to
+            embark on my first professional adventure in the tech industry. My
+            journey so far has been filled with coding, learning, and building
+            projects that I'm proud of. Now, I'm looking for a chance to bring
+            my skills to a forward-thinking, collaborate, grow and create
+            innovative solutions.
           </p>
         </Col>
         <Col className="image-class" md={6}>
           <img src={AboutMe} alt="about-me" className="img-fluid" />
         </Col>
       </Row>
-      <button className="button" onClick={handleButtonClick}>View my work</button>
+      <button className="button" onClick={handleButtonClick}>
+        View my work
+      </button>
     </Container>
-  )
+  );
 }
 
 export default About;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,35 +1,60 @@
-import React from "react";
 import "../styles/portfolio.css";
-
+import React, { useState, useEffect } from "react";
 
 function Portfolio({ data }) {
+  const [displayedText, setDisplayedText] = useState("");
+  const element = "< Welcome to my Portfolio✅/>";
+  const typingSpeed = 100;
 
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setDisplayedText((prev) => prev + element.charAt(index));
+      index++;
+      if (index === element.length) {
+        clearInterval(interval);
+      }
+    }, typingSpeed);
 
-    return (
-        <div className="portfolio-container">
-            <div className="title-header">Welcome to my Portfolio</div>
-            <hr />
-            <p>Check out some of my projects!</p>
-            <div className="card-container">
-                {data.portfolio.map((project, index) => (
-                    <div key={index} className="card">
-                        <a href={project.live} target="_blank" rel="noopener noreferrer">
-                            <img className="image-card" src={project.src} alt={project.title} />
-                        </a>
-                        <div className="card-body">
-                            <div className="title">- {project.title}</div>
-                            <div className="description">{project.description}</div>
-                            <div className="tools">
-                                {project.tools ? project.tools.join(" | ") : ""}
-                            </div>
-                            <button className="github-btn" type="button"
-                                onClick={() => window.open(project.href, "_blank", "noopener, noreferrer")}
-                            >View in GitHub</button>
-                        </div>
-                    </div>
-                ))}
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <div className="portfolio-container">
+      <h1 className="title-header">{displayedText}</h1>
+      <hr />
+      <p>Check out some of my projects!</p>
+      <div className="card-container">
+        {data.portfolio.map((project, index) => (
+          <div key={index} className="card">
+            <a href={project.live} target="_blank" rel="noopener noreferrer">
+              <img
+                className="image-card"
+                src={project.src}
+                alt={project.title}
+              />
+            </a>
+            <div className="card-body">
+              <div className="title">- {project.title}</div>
+              <div className="description">{project.description}</div>
+              <div className="tools">
+                {project.tools ? project.tools.join(" | ") : ""}
+              </div>
+              <button
+                className="github-btn"
+                type="button"
+                onClick={() =>
+                  window.open(project.href, "_blank", "noopener, noreferrer")
+                }
+              >
+                View in GitHub
+              </button>
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Portfolio;
